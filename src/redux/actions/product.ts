f7beaeabc4f820ef034ab9564db93ad73ebe4731
Evgeniya <122976396+evvrod@users.noreply.github.com>
2024-06-05 01:25:02 +0300

    feat(RSS-ECOMM-3_19): Implement Routing and Browser Navigation for Catalog and Product Detail Pages

--- src/redux/actions/product.ts
+++ src/redux/actions/product.ts
@@ -2,5 +2,5 @@ import { createAction } from '@reduxjs/toolkit';
 
 import { ProductData } from '@commercetools/platform-sdk';
 
-export const GET_PRODUCT = createAction<ProductData>('GET_PRODUCTS');
-export const ERROR_GET_PRODUCT = createAction<string>('ERROR_GET_PRODUCTS');
+export const GET_PRODUCT = createAction<ProductData>('GET_PRODUCT');
+export const ERROR_GET_PRODUCT = createAction<string>('ERROR_GET_PRODUCT');
